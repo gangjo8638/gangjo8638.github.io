cbbac0bfb14f57e9eae85184d63d1299df671b38
gangjo8638 <100822045+gangjo8638@users.noreply.github.com>
2024-01-19 20:30:05 +0900

    Update button.js

--- portfolio/js/button.js
+++ portfolio/js/button.js
@@ -1,115 +1,113 @@
+/*
 var ii = 1;
 var getimgno = document.getElementById("boximg");
+
+function updateImage() {
+  getimgno.setAttribute("src", "img2/dju" + ii + ".png");
+}
+
 function clickright() {
-if (ii < 5) {
+  if (ii < 5) {
     ii++;
-    getimgno.setAttribute(
-    "src",
-    "img2/dju" +
-    ii +
-        ".png"
-    );
-    
-    console.log(ii);
-} else if ((ii = 5)) {
+  } else {
     ii = 1;
-    getimgno.setAttribute(
-    "src",
-    "img2/dju" +
-        ii +
-        ".png"
-    );
-    console.log(ii);
+  }
+  updateImage();
+  console.log(ii);
 }
-}
+
 function clickleft() {
-if (ii > 0) {
+  if (ii > 1) {
     ii--;
-    getimgno.setAttribute(
-    "src",
-    "img2/dju" +
-        ii +
-        ".png"
-    );
-    
-    console.log(ii);
-} else if ((ii = 1)) {
+  } else {
     ii = 5;
-    getimgno.setAttribute(
-    "src",
-    "img2/dju" +
-        ii +
-        ".png"
-    );
-    console.log(ii);
-} 
-
+  }
+  updateImage();
+  console.log(ii);
 }
+
 function clickdot(n) {
-getimgno.setAttribute(
-    "src",
-    "img2/dju" + n + ".png"
-);
+  ii = n;
+  updateImage();
 }
 
+// Initial call to set the first image
+updateImage();
+*/
+var ii = 1;
+var getimgno = document.getElementById("boximg");
+
+function updateImage() {
+  getimgno.setAttribute("src", "img2/dju" + ii + ".png");
+}
+
+function clickright() {
+  if (ii < 5) {
+    ii++;
+  } else {
+    ii = 1;
+  }
+  updateImage();
+  console.log(ii);
+}
+
+function clickleft() {
+  if (ii > 1) {
+    ii--;
+  } else {
+    ii = 5;
+  }
+  updateImage();
+  console.log(ii);
+}
+
+function clickdot(n) {
+  ii = n;
+  updateImage();
+}
+
+// 초기 호출로 첫 번째 이미지 설정
+updateImage();
 
 
 
 //2번째
-
 var ii2 = 1;
 var getimgno2 = document.getElementById("boximg2");
-function clickrigh2() {
-if (ii2 < 5) {
-    ii2++;
-    getimgno2.setAttribute(
-    "src",
-    "img2/port" +
-    ii2 +
-        ".png"
-    );
-    
-    console.log(ii2);
-} else if ((ii2 = 5)) {
-    ii2 = 1;
-    getimgno2.setAttribute(
-    "src",
-    "img2/port" +
-        ii2 +
-        ".png"
-    );
-    console.log(ii2);
-}
-}
-function clickleft2() {
-if (ii2 > 0) {
-    ii2--;
-    getimgno2.setAttribute(
-    "src",
-    "img2/port" +
-    ii2 +
-        ".png"
-    );
-    
-    console.log(ii2);
-} else if ((ii2 = 1)) {
-    ii2 = 5;
-    getimgno2.setAttribute(
-    "src",
-    "img2/port" +
-    ii2 +
-        ".png"
-    );
-    console.log(ii2);
-} 
 
+function updateImage2() {
+  getimgno2.setAttribute("src", "img2/port" + ii2 + ".png");
 }
+
+function clickright2() {
+  if (ii2 < 5) {
+    ii2++;
+  } else {
+    ii2 = 1;
+  }
+  updateImage2();
+  console.log(ii2);
+}
+
+function clickleft2() {
+  if (ii2 > 1) {
+    ii2--;
+  } else {
+    ii2 = 5;
+  }
+  updateImage2();
+  console.log(ii2);
+}
+
 function clickdot2(n) {
-    getimgno2.setAttribute(
-    "src",
-    "img2/port" + n + ".png"
-);
+  ii2 = n;
+  updateImage2();
 }
+
+// 초기 호출로 첫 번째 이미지 설정
+updateImage2();
+
+
 
 
 
@@ -118,111 +116,74 @@ function clickdot2(n) {
 
 var ii3 = 1;
 var getimgno3 = document.getElementById("boximg3");
-function clickrigh3() {
-if (ii3 < 5) {
-    ii3++;
-    getimgno3.setAttribute(
-    "src",
-    "img2/ufc" +
-    ii3 +
-        ".png"
-    );
-    
-    console.log(ii3);
-} else if ((ii3 = 5)) {
-    ii3 = 1;
-    getimgno3.setAttribute(
-    "src",
-    "img2/ufc" +
-    ii3 +
-        ".png"
-    );
-    console.log(ii3);
-}
-}
-function clickleft3() {
-if (ii3 > 0) {
-    ii3--;
-    getimgno3.setAttribute(
-    "src",
-    "img2/ufc" +
-    ii3 +
-        ".png"
-    );
-    
-    console.log(ii3);
-} else if ((ii3 = 1)) {
-    ii3 = 5;
-    getimgno3.setAttribute(
-    "src",
-    "img2/ufc" +
-    ii3 +
-        ".png"
-    );
-    console.log(ii3);
-} 
 
+function updateImage3() {
+  getimgno3.setAttribute("src", "img2/ufc" + ii3 + ".png");
 }
+
+function clickright3() {
+  if (ii3 < 5) {
+    ii3++;
+  } else {
+    ii3 = 1;
+  }
+  updateImage3();
+  console.log(ii3);
+}
+
+function clickleft3() {
+  if (ii3 > 1) {
+    ii3--;
+  } else {
+    ii3 = 5;
+  }
+  updateImage3();
+  console.log(ii3);
+}
+
 function clickdot3(n) {
-    getimgno3.setAttribute(
-    "src",
-    "img2/ufc" + n + ".png"
-);
+  ii3 = n;
+  updateImage3();
 }
+
+// 초기 호출로 첫 번째 이미지 설정
+updateImage3();
+
 
 
 //4번째
 
 var ii4 = 1;
 var getimgno4 = document.getElementById("boximg4");
-function clickrigh4() {
-if (ii4 < 5) {
-    ii4++;
-    getimgno4.setAttribute(
-    "src",
-    "img2/game" +
-    ii4 +
-        ".png"
-    );
-    
-    console.log(ii4);
-} else if ((ii4 = 5)) {
-    ii4 = 1;
-    getimgno4.setAttribute(
-    "src",
-    "img2/game" +
-    ii4 +
-        ".png"
-    );
-    console.log(ii4);
-}
-}
-function clickleft4() {
-if (ii4 > 0) {
-    ii4--;
-    getimgno4.setAttribute(
-    "src",
-    "img2/game" +
-    ii4 +
-        ".png"
-    );
-    
-    console.log(ii4);
-} else if ((ii4 = 1)) {
-    ii4 = 5;
-    getimgno4.setAttribute(
-    "src",
-    "img2/game" +
-    ii4 +
-        ".png"
-    );
-    console.log(ii4);
-} 
 
+function updateImage4() {
+  getimgno4.setAttribute("src", "img2/game" + ii4 + ".png");
 }
+
+function clickright4() {
+  if (ii4 < 5) {
+    ii4++;
+  } else {
+    ii4 = 1;
+  }
+  updateImage4();
+  console.log(ii4);
+}
+
+function clickleft4() {
+  if (ii4 > 1) {
+    ii4--;
+  } else {
+    ii4 = 5;
+  }
+  updateImage4();
+  console.log(ii4);
+}
+
 function clickdot4(n) {
-    getimgno4.setAttribute(
-    "src",
-    "img2/game" + n + ".png"
-);
+  ii4 = n;
+  updateImage4();
 }
+
+// 초기 호출로 첫 번째 이미지 설정
+updateImage4();
